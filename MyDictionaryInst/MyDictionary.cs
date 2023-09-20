55ee4018567a6d371cf6edf03107ade8ed675b12
sametion <sametacikk@gmail.com>
2023-09-20 15:38:19 +0300

    dictionary example
    c# taki dictionary yapısına hazır method olmaksızın ekleme meodu yazıldı

---
+++ MyDictionaryInst/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryInst
+{
+    public class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] _key;
+        Tkey[] _tempKey;
+        Tvalue[] _value;
+        Tvalue[] _tempValue;
+        
+        public MyDictionary()
+        {
+                _key = new Tkey[0];
+            _value = new Tvalue[0];
+        }
+        public void Add(Tkey key, Tvalue value) 
+        {
+            _tempKey = _key;
+            _key =new Tkey[_key.Length+1];
+            for(int i = 0; i < _tempKey.Length; i++) 
+            {
+                _key[i] = _tempKey[i];
+            }
+            _key[_key.Length - 1] = key;
+
+            
+            _tempValue = _value;
+            _value=new Tvalue[_value.Length+1];
+
+            for(int i=0; i < _tempValue.Length;i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _value[_value.Length - 1] = value;
+        }
+        public Tkey[] Keys 
+        { 
+            get {return _key ; }
+        }
+        public Tvalue[] Values
+        {
+            get { return _value; } 
+        }
+
+    }
+}
---
+++ MyDictionaryInst/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyDictionaryInst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary= new MyDictionary<int,string>();
+
+            myDictionary.Add(33, "mersin");
+            myDictionary.Add(06, "ankara");
+            myDictionary.Add(34, "istanbul");
+            myDictionary.Add(35, "izmir");
+            myDictionary.Add(43, "kütahya");
+
+            foreach (var item in myDictionary.Keys)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in myDictionary.Values)
+            { 
+                Console.WriteLine(item); 
+            }
+        }
+    }
+}
